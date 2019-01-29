ffa94591fad06aad1e1ec54ae69f44a271b36d27
lasalesi <larsericmueller@gmail.com>
2019-01-29 17:20:58 +0100

    Update setup.py
    Hotfix: setup compatibility to pip 19

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 import re, ast
 
 # get version from __version__ variable in b2bshop/__init__.py
@@ -10,7 +9,8 @@ with open('b2bshop/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
-requirements = parse_requirements("requirements.txt", session="")
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
 
 setup(
 	name='b2bshop',
